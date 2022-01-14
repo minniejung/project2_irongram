61c83361d91aa2d3a79cd52b6d9d2eb4ecbe0194
Oyo1505 <rigoulet.henri.pierre@gmail.com>
2022-01-14 17:54:55 +0100

    test

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.use(
 
 app.use(express.static("public"));
 app.use(logger("dev"));
+//appp
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
