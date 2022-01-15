3eeb982ffa41c802eea71ef09de1fd0c5808dc4a
minniejung <94359917+minniejung@users.noreply.github.com>
2022-01-15 16:27:17 +0100

    Event route, views, forms, model are created

--- app.js
+++ app.js
@@ -53,6 +53,7 @@ app.use(require("./middlewares/exposeLoginStatus"));
 // use routers
 app.use("/", require("./routes/index"));
 app.use("/", require("./routes/users"));
+app.use("/", require("./routes/events"));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
