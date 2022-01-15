893ebf9b219220b58a004b541aea002d4a793055
Melodie Shahrvari <mshahrvari@gmail.com>
2022-01-15 15:13:50 +0100

    Signin , nav, log out created

--- routes/auth.js
+++ routes/auth.js
@@ -14,7 +14,6 @@ router.post("/auth/signup", async (req, res, next) => {
   try {
     const newUser = { ...req.body };
     const foundUser = await user.findOne({ email: newUser.email });
-    res.render("signup");
     if (foundUser) {
       req.flash("error", "Email already regristered");
       res.redirect("/auth/signin");
@@ -28,6 +27,37 @@ router.post("/auth/signup", async (req, res, next) => {
   } catch (err) {
     next(err);
   }
+});
+
+router.post("/auth/signin", async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const foundUser = await user.findOne({ email: email });
+
+    if (!foundUser) {
+      req.flash("error", "Email not regristered");
+      res.redirect("/auth/signup");
+    } else {
+      const pwd = bcrypt.compareSync(password, foundUser.password);
+      if (pwd) {
+        req.flash("success", "Welcome");
+        const userObject = foundUser.toObject();
+        delete userObject.password;
+        req.session.currentUser = userObject;
+        res.redirect("/");
+      } else {
+        req.flash("warning", "wrong password");
+        res.redirect("/auth/signin");
+      }
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/auth/logout", (req, res) => {
+  req.session.destroy();
+  res.redirect("/");
 });
 
 module.exports = router;
