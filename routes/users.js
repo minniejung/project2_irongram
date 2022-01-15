3064d69429de8ee1135d801065be648f52ac35d1
Melodie Shahrvari <mshahrvari@gmail.com>
2022-01-15 16:23:47 +0100

    Profile connected to id

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+router.get("/profile/:id", function (req, res, next) {
+  res.render("profile/:id");
 });
 
 //user
