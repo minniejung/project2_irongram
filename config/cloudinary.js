db5021ee2922e0787eb8b573109c6c8317cef821
Oyo1505 <rigoulet.henri.pierre@gmail.com>
2022-01-18 09:42:02 +0100

    merged branches

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -6,7 +6,7 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_KEY,
   api_secret: process.env.CLOUDINARY_SECRET,
-});
+})
 
 const storage = new CloudinaryStorage({
   cloudinary,
