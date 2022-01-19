d73e59cddc932d69d3931942d706b044cacc1512
Oyo1505 <rigoulet.henri.pierre@gmail.com>
2022-01-19 17:27:19 +0100

    btn like ok

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -17,17 +17,7 @@ hbs.registerHelper("isSelected", function (needle, haystack) {
     : "";
 });
 hbs.registerHelper("isLiked", function (id, array) {
-  console.log(array, id)
-  return Array.isArray(array) ? array.map((e) => e.user_id).includes(id) ?  `class="btn-liked"` :  `class="btn-like55"`: ""
-});
-
-hbs.registerHelper("isLiked", function (id, array) {
-  console.log(array, id);
-  return Array.isArray(array)
-    ? array.map((e) => e.user_id).includes(id)
-      ? `class="btn-like fas fa-heart"`
-      : `class="btn-like far fa-heart"`
-    : "";
+  return  array.includes(id) ?  `class="btn-like fas fa-heart"` :  `class="btn-like far fa-heart`
 });
 
 // add comparison operator feature to hbs templates
